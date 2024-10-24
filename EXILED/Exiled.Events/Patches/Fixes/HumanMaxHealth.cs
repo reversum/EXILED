e7f61ba51f551212f9bc170dfd6f297eda8ca38b
VALERA771 <72030575+VALERA771@users.noreply.github.com>
2024-10-24 21:56:17 +0300

    Halloween 2024 - Health bar (#150)
    * SL version bump
    
    * new effects
    
    * docs
    
    * Map.IsHubertSky
    
    HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT HUBERT
    
    * missing doc
    
    * halloween!!!
    
    * NotFinish
    
    * Fix
    
    * New DamageType
    
    * SilentDamageHandler
    
    * ShakeCamera
    
    * MetalPipe and fix health bug
    
    * changes
    
    * docs + metal pipe
    
    * some updates
    
    * *metal pipe sounds*
    
    * rename
    
    * spawn chance for skeletron
    
    * fix 1
    
    ---------
    
    Co-authored-by: Yamato <66829532+louis1706@users.noreply.github.com>
    Co-authored-by: Misaka_ZeroTwo <45165615+Misaka-ZeroTwo@users.noreply.github.com>
    Co-authored-by: louis1706 <louismonneyron5@yahoo.com>

---
+++ EXILED/Exiled.Events/Patches/Fixes/HumanMaxHealth.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HumanMaxHealth.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Fixes
+{
+#pragma warning disable SA1313
+    using Exiled.API.Features;
+    using HarmonyLib;
+    using Mirror;
+    using PlayerStatsSystem;
+    using UnityEngine;
+
+    /// <summary>
+    /// Fix health bar for custom max hp.
+    /// </summary>
+    [HarmonyPatch(typeof(MaxHealthStat), nameof(MaxHealthStat.WriteValue))]
+    public class HumanMaxHealth
+    {
+#pragma warning disable SA1313
+        private static bool Prefix(MaxHealthStat __instance, NetworkWriter writer)
+        {
+            float value = __instance.CurValue;
+
+            if (Player.Get(__instance.Hub).MaxHealth != default)
+                value -= 100;
+
+            int num = Mathf.Clamp(Mathf.CeilToInt(value), 0, ushort.MaxValue);
+            writer.WriteUShort((ushort)num);
+            return false;
+        }
+    }
+}
