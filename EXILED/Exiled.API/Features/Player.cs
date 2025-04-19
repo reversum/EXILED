75677026aaa8e7fb1ea1f4ab1d062b4b68dc20b4
Nameless <85962933+Misfiy@users.noreply.github.com>
2025-04-19 19:20:16 +0200

    fix: use basegame validation for CustomInfo (#452)
    * Validate using basegame
    
    * Update EXILED/Exiled.API/Features/Player.cs
    
    ---------
    
    Co-authored-by: Yamato <66829532+louis1706@users.noreply.github.com>

--- EXILED/Exiled.API/Features/Player.cs
+++ EXILED/Exiled.API/Features/Player.cs
@@ -348,37 +348,9 @@ namespace Exiled.API.Features
             get => ReferenceHub.nicknameSync.Network_customPlayerInfoString;
             set
             {
-                // NW Client check.
-                if (value.Contains<char>('<'))
+                if (NicknameSync.ValidateCustomInfo(value, out string rejectionText))
                 {
-                    foreach (string token in value.Split('<'))
-                    {
-                        if (token.StartsWith("/", StringComparison.Ordinal) ||
-                            token.StartsWith("b>", StringComparison.Ordinal) ||
-                            token.StartsWith("i>", StringComparison.Ordinal) ||
-                            token.StartsWith("size=", StringComparison.Ordinal) ||
-                            token.Length is 0)
-                            continue;
-
-                        if (token.StartsWith("color=", StringComparison.Ordinal))
-                        {
-                            if (token.Length < 14 || token[13] != '>')
-                                Log.Error($"Custom info of player {Nickname} has been REJECTED. \nreason: (Bad text reject) \ntoken: {token} \nInfo: {value}");
-                            else if (!Misc.AllowedColors.ContainsValue(token.Substring(6, 7)))
-                                Log.Error($"Custom info of player {Nickname} has been REJECTED. \nreason: (Bad color reject) \ntoken: {token} \nInfo: {value}");
-                        }
-                        else if (token.StartsWith("#", StringComparison.Ordinal))
-                        {
-                            if (token.Length < 8 || token[7] != '>')
-                                Log.Error($"Custom info of player {Nickname} has been REJECTED. \nreason: (Bad text reject) \ntoken: {token} \nInfo: {value}");
-                            else if (!Misc.AllowedColors.ContainsValue(token.Substring(0, 7)))
-                                Log.Error($"Custom info of player {Nickname} has been REJECTED. \nreason: (Bad color reject) \ntoken: {token} \nInfo: {value}");
-                        }
-                        else
-                        {
-                            Log.Error($"Custom info of player {Nickname} has been REJECTED. \nreason: (Bad color reject) \ntoken: {token} \nInfo: {value}");
-                        }
-                    }
+                    Log.Error($"Could not set CustomInfo for {Nickname}. Reason: {rejectionText}");
                 }
 
                 InfoArea = string.IsNullOrEmpty(value) ? InfoArea & ~PlayerInfoArea.CustomInfo : InfoArea |= PlayerInfoArea.CustomInfo;
