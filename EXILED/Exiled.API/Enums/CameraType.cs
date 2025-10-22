88887ca73c6c4972fd7afdd4d1f15cbe58b4593f
Yamato <66829532+louis1706@users.noreply.github.com>
2025-10-22 20:31:24 +0200

    Obsolete: CameraType.HczWarheadTop

--- EXILED/Exiled.API/Enums/CameraType.cs
+++ EXILED/Exiled.API/Enums/CameraType.cs
@@ -77,6 +77,7 @@ namespace Exiled.API.Enums
         HczWarheadArmory,
         HczWarheadControl,
         HczWarheadHallway,
+        [System.Obsolete("This Camera no longer exist.")]
         HczWarheadTop,
         #endregion
         #region Lcz
