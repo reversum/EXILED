204b1cd9bd53b33b45bb6c7f69046ae0e09e2635
PUDGE133 <47304666+PUDGE133@users.noreply.github.com>
2025-11-02 19:33:33 +0300

    fix: group(rank) and renaming of some properties (#676)
    * Bug fixes and renaming of some properties to more understandable ones. We need to test this to make sure it doesn't cause problems, because God only knows how it all works internally.
    https://discord.com/channels/656673194693885975/1002713309876854924/1432873468285948055
    
    * Revert "Bug fixes and renaming of some properties to more understandable ones. We need to test this to make sure it doesn't cause problems, because God only knows how it all works internally."
    
    This reverts commit c72eabf3101abc825deef716ac2858f3315e75b1.
    
    * Fix a fix
    
    * Update EXILED/Exiled.API/Features/Player.cs
    
    * Fix RemoteAdminPermissions property
    
    ---------
    
    Co-authored-by: PUDGE <PUDGE@PUDGE>
    Co-authored-by: Yamato <66829532+louis1706@users.noreply.github.com>

--- EXILED/Exiled.API/Features/Player.cs
+++ EXILED/Exiled.API/Features/Player.cs
@@ -564,7 +564,11 @@ namespace Exiled.API.Features
         public PlayerPermissions RemoteAdminPermissions
         {
             get => (PlayerPermissions)ReferenceHub.serverRoles.Permissions;
-            set => ReferenceHub.serverRoles.Permissions = (ulong)value;
+            set
+            {
+                ReferenceHub.serverRoles.Permissions = (ulong)value;
+                ReferenceHub.serverRoles.FinalizeSetGroup();
+            }
         }
 
         /// <summary>
@@ -1915,7 +1919,7 @@ namespace Exiled.API.Features
         }
 
         /// <summary>
-        /// Sets the player's rank.
+        /// Receives an existing rank(group) or, if it doesn't exist, creates a new one and assigns it to this player.
         /// </summary>
         /// <param name="name">The rank name to be set.</param>
         /// <param name="group">The group to be set.</param>
@@ -1923,17 +1927,11 @@ namespace Exiled.API.Features
         {
             if (ServerStatic.PermissionsHandler.Groups.TryGetValue(name, out UserGroup userGroup))
             {
-                userGroup.BadgeColor = group.BadgeColor;
-                userGroup.BadgeText = name;
-                userGroup.HiddenByDefault = !group.Cover;
-                userGroup.Cover = group.Cover;
-
                 ReferenceHub.serverRoles.SetGroup(userGroup, false, false);
             }
             else
             {
                 ServerStatic.PermissionsHandler.Groups.Add(name, group);
-
                 ReferenceHub.serverRoles.SetGroup(group, false, false);
             }
 
